beeed84ed2c2e8f5ce534ddeec797b7fafbb4708
Fabricio Santiago Vallasciani <fabriciosvallasciani@hotmail.com>
2020-06-13 19:28:08 -0300

    updated image field description for alt

--- Models/HolidayHomeImageDto.cs
+++ Models/HolidayHomeImageDto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public string Description { get; set; }
+        public string Alt { get; set; }
 
         public string Url { get; set; }
     }
